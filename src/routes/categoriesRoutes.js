c629ced1a82c10141ac4e473448dd7d931b8c1dc
Dav1Samu3l <daviflorianopolis@gmail.com>
2023-06-23 20:25:40 -0300

    criação dos verbos e rotas

--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -1,44 +1,44 @@
-const { Router } = require('express');
-const router = Router();
-const { Category } = require('../models/Category');
+    const { Router } = require('express');
+    const { Category } = require('../models/category');
+    const router = Router();
 
-router.get('/', async (req, res) => {
-  const categories = await Category.findAll();
-  res.json(categories);
-});
+    router.get('/', async (req, res) => {
+    const categories = await Category.findAll();
+    res.json(categories);
+    });
 
-router.get('/:id', async (req, res) => {
-  const category = await Category.findByPk(req.params.id);
-  if (category) {
+    router.get('/:id', async (req, res) => {
+    const category = await Category.findByPk(req.params.id);
+    if (category) {
+        res.json(category);
+    } else {
+        res.status(404).send('Category not found');
+    }
+    });
+
+    router.post('/', async (req, res) => {
+    const category = await Category.create(req.body);
     res.json(category);
-  } else {
-    res.status(404).send('Category not found');
-  }
-});
+    });
 
-router.post('/', async (req, res) => {
-  const category = await Category.create(req.body);
-  res.json(category);
-});
+    router.put('/:id', async (req, res) => {
+    const category = await Category.findByPk(req.params.id);
+    if (category) {
+        await category.update(req.body);
+        res.json(category);
+    } else {
+        res.status(404).send('Category not found');
+    }
+    });
 
-router.put('/:id', async (req, res) => {
-  const category = await Category.findByPk(req.params.id);
-  if (category) {
-    await category.update(req.body);
-    res.json(category);
-  } else {
-    res.status(404).send('Category not found');
-  }
-});
+    router.delete('/:id', async (req, res) => {
+    const category = await Category.findByPk(req.params.id);
+    if (category) {
+        await category.destroy();
+        res.send('Category deleted');
+    } else {
+        res.status(404).send('Category not found');
+    }
+    });
 
-router.delete('/:id', async (req, res) => {
-  const category = await Category.findByPk(req.params.id);
-  if (category) {
-    await category.destroy();
-    res.send('Category deleted');
-  } else {
-    res.status(404).send('Category not found');
-  }
-});
-
-module.exports = router;
+    module.exports = router;
