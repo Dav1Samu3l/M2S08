98564aca1c93f6f9734cb4385af3a09d3f2310e5
Dav1Samu3l <daviflorianopolis@gmail.com>
2023-06-23 22:23:25 -0300

    [M2S08] Ex 5 - Estagiários - Model

--- src/models/trainee.js
+++ src/models/trainee.js
@@ -1,11 +1,66 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const Trainee = sequelize.define('Trainee', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
-  Trainee.associate = function(models) {
-    // associations can be defined here
-  };
-  return Trainee;
-};
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('./sequelize');
+
+class Trainee extends Model {}
+
+Trainee.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  rg: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  },
+  cpf: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  },
+  primary_phone_contact: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  secondary_phone_contact: {
+    type: DataTypes.STRING
+  },
+  date_birth: {
+    type: DataTypes.DATEONLY,
+    allowNull: false
+  },
+  father_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  mother_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  have_special_needs: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  modelName: 'Trainee'
+});
+
+module.exports = Trainee;
